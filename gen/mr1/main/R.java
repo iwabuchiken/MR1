36749cf281264c277a0d9350aa427591731be18b
Iwabuchi Ken <iwabuchi.k.2010@gmail.com>
2013-03-27 12:39:24 +0900

    B1
    v-1.0
    ---
    1. Get X => Done

--- gen/mr1/main/R.java
+++ gen/mr1/main/R.java
@@ -10,22 +10,66 @@ package mr1.main;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int blue1=0x7f040002;
+        public static final int darkgreen=0x7f040003;
+        public static final int darkolivegreen4=0x7f040005;
+        public static final int gold2=0x7f040007;
+        public static final int gray1=0x7f04000a;
+        public static final int gray_pale=0x7f04000b;
+        public static final int green4=0x7f040004;
+        public static final int purple4=0x7f040006;
+        public static final int red=0x7f040008;
+        /**  Source=http://www.somacon.com/p142.php 
+ main.xml 
+         */
+        public static final int white=0x7f040000;
+        public static final int yello=0x7f040009;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int actv_main_tr_x=0x7f080000;
+        public static final int actv_main_tv_lbl_x=0x7f080001;
+        public static final int actv_main_tv_lbl_y=0x7f080004;
+        public static final int actv_main_tv_lbl_z=0x7f080007;
+        public static final int actv_main_tv_val_x=0x7f080002;
+        public static final int actv_main_tv_val_y=0x7f080005;
+        public static final int actv_main_tv_val_z=0x7f080008;
+        public static final int actv_main_y=0x7f080003;
+        public static final int actv_main_z=0x7f080006;
+        public static final int menu_settings=0x7f080009;
     }
     public static final class layout {
         public static final int actv_main=0x7f030000;
     }
     public static final class menu {
-        public static final int actv_main=0x7f060000;
+        public static final int actv_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        /**  actv_main.xml 
+         */
+        public static final int actv_main_x=0x7f05000e;
+        public static final int actv_main_y=0x7f05000f;
+        public static final int actv_main_z=0x7f050010;
+        public static final int app_name=0x7f050000;
+        public static final int generic_bt_back=0x7f050005;
+        public static final int generic_bt_cancel=0x7f050004;
+        /**  Generic items 
+         */
+        public static final int generic_bt_ok=0x7f050003;
+        public static final int generic_bt_quit=0x7f05000c;
+        public static final int generic_notice=0x7f05000d;
+        public static final int generic_tv_confirm=0x7f050006;
+        public static final int generic_tv_delete=0x7f05000a;
+        public static final int generic_tv_edit=0x7f050009;
+        public static final int generic_tv_menu=0x7f050007;
+        public static final int generic_tv_no_data=0x7f05000b;
+        public static final int generic_tv_register=0x7f050008;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
     }
     public static final class style {
         /** 
@@ -38,10 +82,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/mr1/main/ActvMain.java
+++ src/mr1/main/ActvMain.java
@@ -1,15 +1,133 @@
 package mr1.main;
 
+import java.util.List;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class ActvMain extends Activity {
 
+    private SensorManager sensorManager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.actv_main);
+		
+		this.setTitle(this.getClass().getName());
+		
+//		
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		
+		// Log
+		Log.d("ActvMain.java" + "["
+				+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+				+ ":"
+				+ Thread.currentThread().getStackTrace()[2].getMethodName()
+				+ "]", "onResume()");
+		
+        // �����x�Z���T�[�I�u�W�F�N�g�擾
+        List<Sensor> accelerometerSensors =
+            sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+
+        //debug
+        if (accelerometerSensors != null) {
+			
+        	// Log
+			Log.d("ActvMain.java" + "["
+					+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+					+ ":"
+					+ Thread.currentThread().getStackTrace()[2].getMethodName()
+					+ "]",
+					"accelerometerSensors.size()=" + accelerometerSensors.size());
+        	
+		} else {//if (accelerometerSensors != null)
+			
+			// Log
+			Log.d("ActvMain.java" + "["
+					+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+					+ ":"
+					+ Thread.currentThread().getStackTrace()[2].getMethodName()
+					+ "]", "accelerometerSensors => null");
+			
+		}//if (accelerometerSensors != null)
+		
+        
+        // �����x�Z���T�[�I�u�W�F�N�g���擾�ł����ꍇ
+        if (accelerometerSensors.size() > 0) {
+            // SensorManager�C���X�^���X�ɃZ���T�[�C�x���g���X�i�[��ݒ�
+            sensorManager.registerListener(sensorEventListener,
+                accelerometerSensors.get(0), SensorManager.SENSOR_DELAY_GAME);
+        }
+		
+		
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		
+		// Log
+		Log.d("ActvMain.java" + "["
+				+ Thread.currentThread().getStackTrace()[2].getLineNumber()
+				+ ":"
+				+ Thread.currentThread().getStackTrace()[2].getMethodName()
+				+ "]", "onStart()");
+		
+		setupSensor();
+		
+	}
+
+	private void setupSensor() {
+		// TODO Auto-generated method stub
+		sensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		
+		sensorManager.unregisterListener(sensorEventListener);
+		
 	}
 
 	@Override
@@ -18,5 +136,66 @@ public class ActvMain extends Activity {
 		getMenuInflater().inflate(R.menu.actv_main, menu);
 		return true;
 	}
+
+    private final SensorEventListener sensorEventListener =
+            new SensorEventListener() {
+                // ���[�p�X�t�B���^�p�ϐ�
+                private float lowX;
+                private float lowY;
+                private float lowZ;
+
+                // ���[�p�X�t�B���^�Ώ۔͈�
+                private static final float FILTERING_VALUE = 0.2f;
+
+                // onAccuracyChanged���\�b�h(���x�ύX���C�x���g)
+//                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                    // �����Ȃ�
+                }
+
+                // onSensorChanged���\�b�h(�Z���T�[���ύX���C�x���g)
+//                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    // �Z���T�[�����m�����l���擾
+                    float x = event.values[SensorManager.DATA_X];
+                    float y = event.values[SensorManager.DATA_Y];
+                    float z = event.values[SensorManager.DATA_Z];
+
+                    // ���[�p�X�t�B���^����(����g�U��(��̐k��������ȐU���̉e��)������)
+                    lowX = getLowPassFilterValue(x, lowX);
+                    lowY = getLowPassFilterValue(y, lowY);
+                    lowZ = getLowPassFilterValue(z, lowZ);
+
+                    // Log
+					Log.d("ActvMain.java"
+							+ "["
+							+ Thread.currentThread().getStackTrace()[2]
+									.getLineNumber()
+							+ ":"
+							+ Thread.currentThread().getStackTrace()[2]
+									.getMethodName() + "]", "lowX=" + lowX);
+                    
+                    switch (event.sensor.getType()) {
+
+                    // ���m���ꂽ�Z���T�[��񂪉����x�Z���T�[�̏ꍇ
+                    case Sensor.TYPE_ACCELEROMETER:
+//                        drawableView.effectAccelaration(lowX, lowY, lowZ);
+//
+//                        // ��ʂ��ĕ`��
+//                        drawableView.invalidate();
+                        break;
+
+                    // �����x�Z���T�[�ȊO�͖���
+                    default:
+                        break;
+                    }
+                }
+
+                // ���[�p�X�t�B���^����(����g�U��(��̐k��������ȐU���̉e��)������)
+                private float getLowPassFilterValue(float eventValue, float lowValue) {
+                    return eventValue * FILTERING_VALUE + lowValue
+                        * (1.0f - FILTERING_VALUE);
+                }
+            };
 
 }
